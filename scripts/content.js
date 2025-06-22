a1274dd9a0acd04a0a2bccdf4a03e5db8c7cdcef
Matthew Radford <radfom@rpi.edu>
2025-06-22 16:01:04 -0400

    Added text replacement and clipboard copying and smoother methods for grabbing selected text

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,3 @@
-
 function getMode() {
   return new Promise((resolve) => {
     chrome.storage.sync.get("autocaseMode", (result) => {
@@ -11,101 +10,65 @@ const checkEditor = setInterval(() => {
   const iframe = document.querySelector("#editor_ifr");
   if (iframe?.contentDocument?.readyState === "complete") {
     listenInsideIframe();
+    //applyCaseChange(obj);
     clearInterval(checkEditor);
   }
 }, 500);
 
-
 document.addEventListener("mouseup", () => {
     console.log("Mouseup fired");
 
-    getSelectedText();
+    let obj = getSelectedText().then(applyCaseChange);
+    
 });
 
-function getSelectedText() {
-    const active = document.activeElement;
-    let selection = "";
-    const shadowRoot = active?.shadowRoot;
-    const innerTextArea = shadowRoot?.querySelector('textarea');
-    const host = document.querySelector('faceplate-search-input');
-    const shadow = host?.shadowRoot;
-    const searchBarInput = shadow?.querySelector('input[name="q"]');
-    
-    if (searchBarInput) {
-        selection = searchBarInput.value.substring(input.selectionStart, input.selectionEnd);
-        getMode().then((mode) => {
-            const transformed = applyCasing(selection, mode);
-            if (transformed) {
-                 console.log("Reddit search bar text selected: ", selection);
-                console.log("Transformed to ", mode, " :", transformed);
-            }
-        });
+async function listenInsideIframe() {
+    let obj = {
+        type: '',
+        element: null,
+        text: ""
     }
+    let mode = await getMode();
+  const iframes = [...document.querySelectorAll('iframe')];
+  iframes.forEach((iframe) => {
+    try {
+        const doc = iframe?.contentDocument || iframe?.contentWindow?.document;
+        const win = iframe.contentWindow;
 
-    else if (innerTextArea) {
-        selection = innerTextArea.value.substring(innerTextArea.selectionStart, innerTextArea.selectionEnd);
-        getMode().then((mode) => {
-            const transformed = applyCasing(selection, mode);
-            if (transformed) {
-                console.log("Reddit editable title selected: ", selection);
-                console.log("Transformed to ", mode, " :", transformed);
-            }
-        });
+        if (doc && win) {
+            doc.addEventListener('mouseup', () => {
+                const selected = win.getSelection()?.toString().trim();
+                const anchor = win.getSelection().anchorNode;
+                const isEdit = anchor?.parentElement?.closest('input, textarea, [contenteditable="true"]');
+                if (selected) {
+                    if (isEdit) {
+                        console.log("You selected inside an editable iframe: ", selected);
+                        let obj = {
+                            type: 'iframe-edit',
+                            element: isEdit,
+                            text: applyCasing(selected, mode)
+                        }
+                        applyCaseChange(obj);
+                    }
+                    else {
+                        let obj = {
+                            type: 'iframe-nonedit',
+                            element: iframe,
+                            text: applyCasing(selected, mode)
+                        }
+                        console.log("You selected inside an iframe: ", selected);
+                        applyCaseChange(obj);
+                    }
+                }
+            });
+        }
     }
-    
-   else if (active && (active.tagName == "INPUT" || active.tagName == "TEXTAREA")) {
-        selection = active.value.substring(active.selectionStart, active.selectionEnd);
-        
-        getMode().then((mode) => {
-            const transformed = applyCasing(selection, mode);
-            if (transformed) {
-                console.log("You selected: ", selection, " in an input/text_area");
-                console.log("Transformed to ", mode, " :", transformed);
-            }
-        });
-        
+    catch (err) {
+        console.log("Cross-origin iframe or iframe not loaded yet...");
     }
-    else if (active?.closest('[contenteditable="true"]')) {
-        selection = window.getSelection()?.toString().trim();
-        
-        getMode().then((mode) => {
-            const transformed = applyCasing(selection, mode);
-            if (transformed) {
-                console.log("You selected:", selection, "in an editable element of some sort (Reddit)");
-                console.log("Transformed to ", mode, " :", transformed);
-            }
-        });
-    }
+  })
 
-    else {
-        selection = window.getSelection()?.toString().trim();
-        getMode().then((mode) => {
-            const transformed = applyCasing(selection, mode);
-            if (transformed) {
-                console.log("You selected: ", selection);
-                console.log("Transformed to ", mode, " :", transformed);
-            }
-        });
-        
-    }
-    
-   
-
-
-}
-
-function listenInsideIframe() {
-  const iframe = document.querySelector("#editor_ifr");
-  const doc = iframe?.contentDocument || iframe?.contentWindow?.document;
-
-  if (doc) {
-    doc.addEventListener("mouseup", () => {
-      const selected = doc.getSelection()?.toString().trim();
-      if (selected) {
-        console.log("You selected inside TinyMCE:", selected);
-      }
-    });
-  }
+  
 }
 
 function applyCasing(selectedText, mode) {
@@ -140,5 +103,135 @@ function toSentenceCase(str) {
   return ret;
 }
 
+async function getSelectedText() {
+    let obj =  {
+        type: '',
+        element: null,
+        text: ""
+    };
 
+    let selection = "";
+    const active = document.activeElement;
+    const shadowRoot = active?.shadowRoot;
+    const innerTextArea = shadowRoot?.querySelector('textarea, input');
+    const editable = shadowRoot?.activeElement?.closest('[contenteditable="true"]') || 
+        shadowRoot?.querySelector('[contenteditable="true"]');
 
+    const mode = await getMode();
+
+    if (active && (active.tagName == "INPUT" || active.tagName == "TEXTAREA")) {
+        // normal input/textarea
+        selection = active.value.substring(active.selectionStart, active.selectionEnd);
+
+        const transformed = applyCasing(selection, mode);
+        if (transformed) {
+            console.log("Input field or textarea selected: ", selection);
+            console.log("Transformed to ", mode, " :", transformed);
+            obj = {
+                type: 'input-textarea',
+                element: active,
+                text: transformed
+            }
+            return obj;
+        }
+    }
+    else if (innerTextArea) {
+        // shadowDOM input/textarea
+        selection = innerTextArea.value.substring(innerTextArea.selectionStart, innerTextArea.selectionEnd);
+
+        const transformed = applyCasing(selection, mode);
+        if (transformed) {
+            console.log("Input field or textarea selected within shadow DOM: ", selection);
+            console.log("Transformed to ", mode, " :", transformed);
+            obj = {
+                type: 'shadow-input',
+                element: innerTextArea,
+                text: transformed
+            }
+            return obj;
+        }
+    }
+    else if (editable) {
+        // editable but in shadow DOM
+        selection = shadowRoot.getSelection()?.toString().trim();
+        
+        const transformed = applyCasing(selection, mode);
+        if (transformed) {
+            console.log("Some editable field within shadow DOM: ", selection);
+            console.log("Transformed to ", mode, " :", transformed);
+            obj = {
+                type: 'shadow-editable',
+                element: editable,
+                text: transformed
+            }
+            return obj;
+         }
+    }
+    else if (active?.closest('[contenteditable="true"]')) {
+        // editable text wrapped somewhere in contenteditable=true
+        selection = window.getSelection()?.toString().trim();
+        
+        const transformed = applyCasing(selection, mode);
+        if (transformed) {
+            console.log("Some editable field: ", selection);
+            console.log("Transformed to ", mode, " :", transformed);
+            obj = {
+                type: 'editable',
+                element: active,
+                text: transformed
+            }
+            return obj;
+        }
+    }
+    else { 
+        // non-editable text
+        selection = window.getSelection()?.toString().trim();
+        const transformed = applyCasing(selection, mode);
+        if (transformed) {
+            console.log("Some non-editable field: ", selection);
+            console.log("Transformed to ", mode, " :", transformed);
+            obj = {
+                type: 'noneditable',
+                element: active,
+                text: transformed
+            }
+            return obj;
+        }
+    }
+
+}
+
+function applyCaseChange(object) {
+    if (!object) return;
+    const {type, element, text} = object;
+
+    if (!element || text == "" || type == '') return;
+
+    if (type == 'input-textarea' || type == 'shadow-input') {
+        if (element.selectionStart != null) {
+            const start = element.selectionStart;
+            const end = element.selectionEnd;
+            const before = element.value.slice(0, start);
+            const after = element.value.slice(end);
+            element.value = before + text + after;
+            element.setSelectionRange(start, start + text.length);
+        }
+    }
+    else if (type == 'editable' || type == 'shadow-editable' || type == 'iframe-edit' ) {
+        const root = element.getRootNode?.() || document;
+        const sel = root.getSelection?.();
+        if (!sel || sel.rangeCount == 0) return;
+        const success = document.execCommand?.('insertText', false, text);
+        const range = sel.getRangeAt(0);
+        if (!success) {
+            range.deleteContents();
+            range.insertNode(document.createTextNode(text));
+        }
+        
+    }
+    else {
+        navigator.clipboard.writeText(text).then(() => {
+            console.log("Copied to clipboard: ", text);
+        })
+    }
+}
